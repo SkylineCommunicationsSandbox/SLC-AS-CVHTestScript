fe5dc7d1530953a9f3e0c5b758cffb21bf9bd69f
Conan Van Hove <conan.van.hove@skyline.be>
2023-09-19 16:21:46 +0200

    do it

--- CVHTestScript_1/CVHTestScript_1.cs
+++ CVHTestScript_1/CVHTestScript_1.cs
@@ -17,7 +17,7 @@ namespace CVHTestScript_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello there world too");
+			engine.GenerateInformation("Hello there world too 2");
 	
 		}
 	}
